9c3b5ce4d819582a21a02dfdd0a633524956ad01
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-10-24 14:06:58 +0200

    Clear the screen at each loop iteration before rerendering it

--- src/main.cpp
+++ src/main.cpp
@@ -51,6 +51,8 @@ int main() {
 		while(!glfwWindowShouldClose(window)) {
 			glfwPollEvents();
 			processInput(window);
+			glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
 			glfwSwapBuffers(window);
 		}
 	}
