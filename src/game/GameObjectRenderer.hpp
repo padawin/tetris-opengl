c00fde5a7e5927005912290cd95b12f62f47c649
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-11-04 22:51:04 +0100

    a GamObjectRenderer needs abstract methods to set its geometry

--- src/game/GameObjectRenderer.hpp
+++ src/game/GameObjectRenderer.hpp
@@ -5,6 +5,10 @@ class GameObjectRenderer {
 	public:
 	virtual ~GameObjectRenderer() {}
 	virtual void render() = 0;
+
+	virtual void setScale(float x, float y, float z) = 0;
+	virtual void setRotation(float x, float y, float z) = 0;
+	virtual void setPosition(float x, float y, float z) = 0;
 };
 
 #endif
